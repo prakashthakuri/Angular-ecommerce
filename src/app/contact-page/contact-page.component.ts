b314fa1da35a315ece30bec2071c95cfe0cce146
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-09-04 21:30:37 -0400

    Creating Contact-Page and Static Forms

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,3 +1,6 @@
+import { fakeListings } from './../fake-data';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from './../types';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  email: string = ''
+  message: string = ''
+  listing: Listing
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.listing = fakeListings.find(listing =>listing.id === id)
+    this.message = `Hi, I am inetresred in your ${this.listing.name}`
+  }
+
+  //shows the alert mesages and send us back to the listing page
+  
+  sendMessage() :void {
+    alert(' Your Message has been sent!')
+    this.router.navigateByUrl('/listings')
   }
 
 }
