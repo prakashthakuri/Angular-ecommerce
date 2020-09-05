9a57bfbb6588aad2aa2c0f2e6c1dc6fb6b66255b
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-09-04 21:38:45 -0400

    Creating My Listing with Edit and Delete Alert

--- src/app/my-listing-page/my-listing-page.component.ts
+++ src/app/my-listing-page/my-listing-page.component.ts
@@ -1,15 +1,22 @@
+import { fakeMyListings } from './../fake-data';
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-my-listing-page',
   templateUrl: './my-listing-page.component.html',
-  styleUrls: ['./my-listing-page.component.css']
+  styleUrls: ['./my-listing-page.component.css'],
 })
 export class MyListingPageComponent implements OnInit {
+  listings: Listing[] = []
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
 
+    this.listings = fakeMyListings
+  }
+  onDeletedClicked(listingId: string):void { //alert only
+    alert(`Your listing is deleted`)
+  }
 }
