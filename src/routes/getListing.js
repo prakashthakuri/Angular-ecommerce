bd263c9449f6c9f460d5ddb7bcc3b9df5c2610c5
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-09-07 12:49:29 -0400

    Handling errors in Hapi using hapi/boom

--- src/routes/getListing.js
+++ src/routes/getListing.js
@@ -1,4 +1,5 @@
 import { fakeListings } from "./fake-data"
+import  Boom  from "@hapi/boom"
 
 
 
@@ -7,6 +8,11 @@ export const getListingRoute = {
     path: '/api/listings/{id}',
     handler: (req, h) => {
         const id = req.params.id
-        return fakeListings.find(listing => listing.id === id)
+        
+        const listing = fakeListings.find(listing => listing.id === id)
+        if(!listing) {
+            throw Boom.notFound(`Listing does not exisit with ${id}`)
+        }
+            return listing
     }
 }
