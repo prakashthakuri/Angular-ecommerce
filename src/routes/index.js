fd536716692f753dd95840442010856a478244b8
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-09-17 19:05:20 -0400

    Update Listing Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
 import { addViewToListingRoute } from "./addViewToListing";
+import { createNewListingRoute } from "./createNewListing";
 import { getAllListingsRoute } from "./getAllListing";
 import { getListingRoute } from "./getListing";
 import { getUserListingRoute } from "./getUserListing";
+import { UpdateListingRoute } from "./updateListing";
 
 
 export default [
     getAllListingsRoute,
     getListingRoute,
     addViewToListingRoute,
-    getUserListingRoute
+    getUserListingRoute,
+    createNewListingRoute,
+    UpdateListingRoute,
+
 ]
